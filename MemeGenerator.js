67b53c80edc68eddb9080aea7204075d8673f931
Nivisiru <143357249+Nivisiru@users.noreply.github.com>
2023-09-18 19:40:33 -0700

    Update MemeGenerator.js

--- MemeGenerator.js
+++ MemeGenerator.js
@@ -5,29 +5,24 @@ document.addEventListener('DOMContentLoaded', function () {
     memeForm.addEventListener('submit', function (e) {
         e.preventDefault();
 
-        // Get form input values
         const topText = document.getElementById('top-text').value;
         const bottomText = document.getElementById('bottom-text').value;
         const imageUrl = document.getElementById('image-url').value;
 
-        // Validate inputs
         if (!topText || !bottomText || !imageUrl) {
             alert('Please fill out all fields.');
             return;
         }
 
-        // Create a meme div
         const memeDiv = document.createElement('div');
         memeDiv.className = 'meme';
 
-        // Create an image element
         const imgContainer = document.createElement('div');
         imgContainer.className = 'image-container';
 
         const img = document.createElement('img');
         img.src = imageUrl;
 
-        // Create delete button
         const deleteButton = document.createElement('button');
         deleteButton.className = 'delete-button';
         deleteButton.innerText = 'Delete';
@@ -35,27 +30,22 @@ document.addEventListener('DOMContentLoaded', function () {
             memeDiv.remove();
         });
 
-        // Create top text
         const topTextDiv = document.createElement('div');
         topTextDiv.className = 'top-text';
         topTextDiv.innerText = topText;
 
-        // Create bottom text
         const bottomTextDiv = document.createElement('div');
         bottomTextDiv.className = 'bottom-text';
         bottomTextDiv.innerText = bottomText;
 
-        // Append elements
         imgContainer.appendChild(img);
         memeDiv.appendChild(imgContainer);
         memeDiv.appendChild(deleteButton);
         memeDiv.appendChild(topTextDiv);
         memeDiv.appendChild(bottomTextDiv);
 
-        // Insert meme div at the beginning of the container
         memeContainer.insertBefore(memeDiv, memeContainer.firstChild);
 
-        // Clear form inputs
         memeForm.reset();
     });
 });
